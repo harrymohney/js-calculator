d05bb6d5e3f7c94768ff033e8d7e64f8ff8372b9
harryhmohney <harrison.mohney@gmail.com>
2024-02-08 18:20:08 -0800

    js refactor

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Attach event listeners
+  document.getElementById("allClear").addEventListener("click", clearScreen);
+  document.getElementById("clear").addEventListener("click", clearScreen);
+  document.getElementById("percent").addEventListener("click", function() {
+      myFunction("percent");
+  });
+  document.getElementById("divide").addEventListener("click", function() {
+      myFunction("divide");
+  });
+  document.getElementById("multiply").addEventListener("click", function() {
+      myFunction("multiply");
+  });
+  document.getElementById("subtract").addEventListener("click", function() {
+      myFunction("subtract");
+  });
+  document.getElementById("add").addEventListener("click", function() {
+      myFunction("add");
+  });
+  document.getElementById("equals").addEventListener("click", calculate);
+  // Attach event listeners for other buttons here
+});
+
 // Determines button clicked via id
 function myFunction(id) {
-  document.calc.result.value+=id;
+  document.calculator.result.value += id;
 }
 
 // Clears calculator input screen
 function clearScreen() {
-  document.calc.result.value="";
+  document.calculator.result.value = "";
 }
 
 // Calculates input values
 function calculate() {
   try {
-    var input = eval(document.calc.result.value);
-    document.calc.result.value=input;
-  } catch(err){
-      document.calc.result.value="Error";
-    }
+      var input = eval(document.calculator.result.value);
+      document.calculator.result.value = input;
+  } catch (err) {
+      document.calculator.result.value = "Error";
+  }
 }
